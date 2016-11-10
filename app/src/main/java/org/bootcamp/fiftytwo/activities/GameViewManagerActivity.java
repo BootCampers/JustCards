aa6b9b44e84e734df7132d34dab36bc5c76ee551
Baphna <baphna@qti.qualcomm.com>
2016-11-10 06:28:32 -0800

    Dummy activites and fragments

---
+++ app/src/main/java/org/bootcamp/fiftytwo/activities/GameViewManagerActivity.java
@@ -0,0 +1,31 @@
+package org.bootcamp.fiftytwo.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import org.bootcamp.fiftytwo.R;
+
+public class GameViewManagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_view_manager);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/org/bootcamp/fiftytwo/activities/RegisterActivity.java
@@ -0,0 +1,15 @@
+package org.bootcamp.fiftytwo.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.bootcamp.fiftytwo.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
