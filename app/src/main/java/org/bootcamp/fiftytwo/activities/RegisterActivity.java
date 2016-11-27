132046daa7c4a9ff648c26345db8e8dea6e7bc72
Amit Goenka <master.goenka@gmail.com>
2016-11-27 11:03:35 -0600

    Refactoring Register Activity Class, this screen is very slow.

--- app/src/main/java/org/bootcamp/fiftytwo/activities/RegisterActivity.java
+++ app/src/main/java/org/bootcamp/fiftytwo/activities/RegisterActivity.java
@@ -30,6 +30,10 @@ import org.bootcamp.fiftytwo.utils.Constants;
 import org.bootcamp.fiftytwo.utils.NetworkUtils;
 import org.bootcamp.fiftytwo.utils.PlayerUtils;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 import static org.bootcamp.fiftytwo.utils.Constants.DISPLAY_NAME;
 import static org.bootcamp.fiftytwo.utils.Constants.USER_AVATAR_URI;
 import static org.bootcamp.fiftytwo.utils.Constants.USER_ID;
@@ -38,12 +42,12 @@ import static org.bootcamp.fiftytwo.utils.Constants.USER_PREFS;
 public class RegisterActivity extends AppCompatActivity {
 
     String userAvatarURI = "";
-    EditText usernameTextBox;
-    ImageView avatarImageView;
-    FloatingActionButton browseButton;
-    Button registerButton;
-    ScrollView scrollView;
-    RelativeLayout networkFailureBanner;
+    @BindView(R.id.userName) EditText usernameTextBox;
+    @BindView(R.id.ivAvatar) ImageView avatarImageView;
+    @BindView(R.id.edit_fab) FloatingActionButton browseButton;
+    @BindView(R.id.registerBttn) Button registerButton;
+    @BindView(R.id.register_form) ScrollView scrollView;
+    @BindView(R.id.networkFailureBanner) RelativeLayout networkFailureBanner;
     User user = null;
     SharedPreferences sharedPreferences;
 
@@ -51,6 +55,7 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        ButterKnife.bind(this);
 
         if (ParseUser.getCurrentUser() != null) {
             startWithCurrentUser();
@@ -58,7 +63,8 @@ public class RegisterActivity extends AppCompatActivity {
             loginToParse();
         }
 
-        instantiateWidgets();
+        userAvatarURI = PlayerUtils.getDefaultAvatar();
+        loadAvatar(userAvatarURI);
     }
 
     @Override
@@ -92,31 +98,45 @@ public class RegisterActivity extends AppCompatActivity {
         if (requestCode == Constants.PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null) {
             userAvatarURI = data.getStringExtra(Constants.SELECTED_AVATAR);
             Log.d(Constants.TAG, userAvatarURI);
-            Glide.with(this)
-                    .load(userAvatarURI)
-                    .asBitmap()
-                    .centerCrop()
-                    .into(new BitmapImageViewTarget(avatarImageView) {
-                        @Override
-                        protected void setResource(Bitmap resource) {
-                            RoundedBitmapDrawable circularBitmapDrawable = RoundedBitmapDrawableFactory.create(getResources(), resource);
-                            circularBitmapDrawable.setCircular(true);
-                            avatarImageView.setImageDrawable(circularBitmapDrawable);
-                        }
-                    });
+            loadAvatar(userAvatarURI);
         }
     }
 
-    private void instantiateWidgets() {
-        usernameTextBox = (EditText) findViewById(R.id.userName);
-        avatarImageView = (ImageView) findViewById(R.id.ivAvatar);
-        browseButton = (FloatingActionButton) findViewById(R.id.edit_fab);
-        registerButton = (Button) findViewById(R.id.registerBttn);
-        scrollView = (ScrollView) findViewById(R.id.register_form);
-        networkFailureBanner = (RelativeLayout) findViewById(R.id.networkFailureBanner);
+    @OnClick(R.id.edit_fab)
+    public void browse() {
+        Intent intent = new Intent(RegisterActivity.this, AvatarSelectionActivity.class);
+        startActivityForResult(intent, Constants.PICK_IMAGE_REQUEST, null);
+    }
 
-        userAvatarURI = PlayerUtils.getDefaultAvatar();
+    @OnClick(R.id.registerBttn)
+    public void register() {
+        String username = usernameTextBox.getText().toString();
+        String usernameSansWhiteSpace = username.replaceAll("\\s+", "");
+        if (usernameSansWhiteSpace.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Username must have a value!", Toast.LENGTH_SHORT).show();
+            usernameTextBox.requestFocus();
+            scrollView.scrollTo(usernameTextBox.getScrollX(), usernameTextBox.getScrollY());
+            return;
+        }
 
+        user = new User(userAvatarURI, username);
+
+        SharedPreferences userPrefs = getSharedPreferences(USER_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = userPrefs.edit();
+        editor.putString(DISPLAY_NAME, username);
+        editor.putString(USER_AVATAR_URI, user.getAvatarUri());
+        editor.putString(USER_ID, ParseUser.getCurrentUser().getObjectId());
+        editor.apply();
+
+        Intent createGameIntent = new Intent(RegisterActivity.this, SelectGameActivity.class);
+        createGameIntent.putExtra(USER_AVATAR_URI, user.getAvatarUri());
+        createGameIntent.putExtra(DISPLAY_NAME, user.getDisplayName());
+        createGameIntent.putExtra(USER_ID, ParseUser.getCurrentUser().getObjectId());
+        finish();
+        startActivity(createGameIntent);
+    }
+
+    private void loadAvatar(String userAvatarURI) {
         Glide.with(this)
                 .load(userAvatarURI)
                 .asBitmap()
@@ -129,44 +149,6 @@ public class RegisterActivity extends AppCompatActivity {
                         avatarImageView.setImageDrawable(circularBitmapDrawable);
                     }
                 });
-
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String username = usernameTextBox.getText().toString();
-                String usernameSansWhiteSpace = username.replaceAll("\\s+", "");
-                if (usernameSansWhiteSpace.isEmpty()) {
-                    Toast.makeText(getApplicationContext(), "Username must have a value!", Toast.LENGTH_SHORT).show();
-                    usernameTextBox.requestFocus();
-                    scrollView.scrollTo(usernameTextBox.getScrollX(), usernameTextBox.getScrollY());
-                    return;
-                }
-
-                user = new User(userAvatarURI, username);
-
-                SharedPreferences userPrefs = getSharedPreferences(USER_PREFS, MODE_PRIVATE);
-                SharedPreferences.Editor editor = userPrefs.edit();
-                editor.putString(DISPLAY_NAME, username);
-                editor.putString(USER_AVATAR_URI, user.getAvatarUri());
-                editor.putString(USER_ID, ParseUser.getCurrentUser().getObjectId());
-                editor.apply();
-
-                Intent createGameIntent = new Intent(RegisterActivity.this, SelectGameActivity.class);
-                createGameIntent.putExtra(USER_AVATAR_URI, user.getAvatarUri());
-                createGameIntent.putExtra(DISPLAY_NAME, user.getDisplayName());
-                createGameIntent.putExtra(USER_ID, ParseUser.getCurrentUser().getObjectId());
-                finish();
-                startActivity(createGameIntent);
-            }
-        });
-
-        browseButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(RegisterActivity.this, AvatarSelectionActivity.class);
-                startActivityForResult(intent, Constants.PICK_IMAGE_REQUEST, null);
-            }
-        });
     }
 
     private void loginToParse() {
