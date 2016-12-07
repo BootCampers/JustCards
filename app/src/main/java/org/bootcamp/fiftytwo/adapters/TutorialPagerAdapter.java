6bf0bbb08cabdb318f90063a91442b98eef7902e
Ankit Baphna <anki.cruise@gmail.com>
2016-12-07 14:19:59 -0800

    Seperating tutorial clases

---
+++ app/src/main/java/org/bootcamp/fiftytwo/adapters/TutorialPagerAdapter.java
@@ -0,0 +1,35 @@
+package org.bootcamp.fiftytwo.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import org.bootcamp.fiftytwo.fragments.TutorialPageDetailFragment;
+
+/**
+ * Created by baphna on 12/7/2016.
+ */
+
+public class TutorialPagerAdapter extends FragmentPagerAdapter{
+
+    public TutorialPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // getItem is called to instantiate the fragment for the given page.
+        return TutorialPageDetailFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public int getCount() {
+        // Show 3 total pages.
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return String.valueOf(position+1);
+    }
+}
--- app/src/main/java/org/bootcamp/fiftytwo/fragments/TutorialFragment.java
+++ app/src/main/java/org/bootcamp/fiftytwo/fragments/TutorialFragment.java
@@ -4,20 +4,18 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import org.bootcamp.fiftytwo.R;
+import org.bootcamp.fiftytwo.adapters.TutorialPagerAdapter;
 
 import github.chenupt.springindicator.SpringIndicator;
 
-import static org.bootcamp.fiftytwo.utils.Constants.ARG_CARD_COUNT;
-import static org.bootcamp.fiftytwo.utils.Constants.ARG_PLAYER_COUNT;
+import static org.bootcamp.fiftytwo.utils.Constants.ARG_FROM_PAGE;
+import static org.bootcamp.fiftytwo.utils.Constants.ARG_TO_PAGE;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,12 +27,14 @@ public class TutorialFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    private SectionsPagerAdapter mSectionsPagerAdapter;
+    private TutorialPagerAdapter mSectionsPagerAdapter;
 
     /**
      * The {@link ViewPager} that will host the section contents.
      */
     private ViewPager mViewPager;
+    private int fromPage;
+    private int toPage;
 
     public TutorialFragment() {
         // Required empty public constructor
@@ -43,8 +43,8 @@ public class TutorialFragment extends Fragment {
     public static TutorialFragment newInstance(int fromPage, int toPage) {
         TutorialFragment fragment = new TutorialFragment();
         Bundle args = new Bundle();
-        args.putInt(ARG_PLAYER_COUNT, fromPage);
-        args.putInt(ARG_CARD_COUNT, toPage);
+        args.putInt(ARG_FROM_PAGE, fromPage);
+        args.putInt(ARG_TO_PAGE, toPage);
         fragment.setArguments(args);
         return fragment;
     }
@@ -53,7 +53,8 @@ public class TutorialFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            getArguments().getInt(ARG_PLAYER_COUNT);
+            fromPage = getArguments().getInt(ARG_FROM_PAGE);
+            toPage = getArguments().getInt(ARG_TO_PAGE);
         }
     }
 
@@ -63,7 +64,7 @@ public class TutorialFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_tutorial, container, false);
 
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getChildFragmentManager());
+        mSectionsPagerAdapter = new TutorialPagerAdapter(getChildFragmentManager());
 
         // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) view.findViewById(R.id.container);
@@ -112,77 +113,5 @@ public class TutorialFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.item_tutorial, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "SECTION 1";
-                case 1:
-                    return "SECTION 2";
-                case 2:
-                    return "SECTION 3";
-            }
-            return null;
-        }
     }
 }
