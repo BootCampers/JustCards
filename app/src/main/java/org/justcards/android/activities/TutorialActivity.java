00812975114f4d8c6c244941148e423f9a8dde30
Ankit Baphna <anki.cruise@gmail.com>
2017-06-10 18:47:02 -0700

    Tutorial navigation instructions

--- app/src/main/java/org/justcards/android/activities/TutorialActivity.java
+++ app/src/main/java/org/justcards/android/activities/TutorialActivity.java
@@ -2,6 +2,8 @@ package org.justcards.android.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 import org.justcards.android.R;
 
@@ -9,6 +11,8 @@ public class TutorialActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial);
     }
